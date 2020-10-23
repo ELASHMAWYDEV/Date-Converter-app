e485947bee618a72d0b8a93094acbda538e2a455
ELASHMAWY DEV <contact@elashmawydev.com>
2020-10-23 23:07:38 +0200

    first commit

---
+++ app/assets/Colors.js
@@ -0,0 +1,6 @@
+const Colors = {
+  primary: "#29617f",
+  white: "#FFFFFF"
+}
+
+export default Colors;
---
+++ config.js
@@ -0,0 +1,26 @@
+export const EMAIL = "appother.email@gmail.com";
+export const GOOGLE_PLAY_URL =
+  "https://play.google.com/store/apps/details?id=com.techview.dateconverter";
+export const APP_STORE_URL = "";
+export const DATE_URL = "http://date.app-other.com/";
+export const AZKAR_URL = "http://azkar.app-other.com/";
+
+//Admob Ads for Android
+export const INTERSTITIAL_UNIT_ID_ANDROID =
+  "ca-app-pub-4168953325851257/8649380160";
+export const BANNER_UNIT_ID_ANDROID = "ca-app-pub-4168953325851257/6023216823";
+
+//Admob Ads for IOS
+export const INTERSTITIAL_UNIT_ID_IOS =
+  "ca-app-pub-4168953325851257/6753844567";
+export const BANNER_UNIT_ID_IOS = "ca-app-pub-4168953325851257/9056751882";
+
+//Admob Test ids
+//IOS
+export const INTERSTITIAL_TEST_ID_IOS =
+  "ca-app-pub-3940256099942544/4411468910";
+export const BANNER_TEST_ID_IOS = "ca-app-pub-3940256099942544/2934735716";
+//Android
+export const INTERSTITIAL_TEST_ID_ANDROID =
+  "ca-app-pub-3940256099942544/1033173712";
+export const BANNER_TEST_ID_ANDROID = "ca-app-pub-3940256099942544/6300978111";
